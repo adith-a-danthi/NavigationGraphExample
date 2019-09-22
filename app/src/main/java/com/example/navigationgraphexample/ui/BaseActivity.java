b0e3ab2007ed8e9e6f678fbd902349aa1e0f4e11
Adith A Danthi <adith.danthi@gmail.com>
2019-09-22 12:17:45 +0530

    Added two fragments

--- app/src/main/java/com/example/navigationgraphexample/ui/BaseActivity.java
+++ app/src/main/java/com/example/navigationgraphexample/ui/BaseActivity.java
@@ -1,8 +1,10 @@
-package com.example.navigationgraphexample;
+package com.example.navigationgraphexample.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.navigationgraphexample.R;
 
 public class BaseActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/com/example/navigationgraphexample/ui/fragments/SettingsFragment.java
@@ -0,0 +1,51 @@
+package com.example.navigationgraphexample.ui.fragments;
+
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.navigationgraphexample.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SettingsFragment extends Fragment {
+
+
+    public SettingsFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_settings, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        BottomNavigationView bottomNavigationView = getActivity().findViewById(R.id.bottomNav);
+
+        NavController navController =
+                Navigation.findNavController(requireActivity(),R.id.base_fragment);
+
+        NavigationUI.setupWithNavController(bottomNavigationView,navController);
+    }
+
+}
